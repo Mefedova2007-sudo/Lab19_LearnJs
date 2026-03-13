d7a6f01733746838e5e1224bac2ae43600874bfb
K1eranza <zubrevaevelina20641@gmail.com>
2026-03-13 11:09:12 +0300

    Step 5: loops in JavaScript

---
+++ main.js
@@ -0,0 +1,56 @@
+for (let i = 0 ; i < 5 ; i++) {
+console.log(i);
+}
+for (let i = 1; i <= 10; i++) {
+    numbers1to10 += i + " ";
+}
+for (let i = 1; i <= 10; i++) {
+    if (i % 2 === 0) { 
+        evenNumbers += i + " ";
+    }
+}
+let sum = 0;
+for (let i = 1; i <= 10; i++) {
+    sum += i;  
+}
+let count = 0;
+while (count < 3) {
+console.log("Count:", count);
+count++;
+}
+  let number = 5;
+        let result = "Значения: ";
+        while (number >= 0) {
+            result += number + " ";
+            number--;
+        }
+        document.getElementById('output').textContent = result;
+ console.log(result);
+ let doValue = 0;
+do {
+console.log("Value:", doValue);
+doValue++;
+} while (doValue < 3);
+
+for (let i = 1; i <= 10; i++) {
+
+if (i === 5) {
+   continue; 
+}
+if (i === 8) {
+    break; 
+}
+console.log(i);
+}
+for (let i = 1; i <= 3; i++) {
+for (let j = 1 ; j <= 3; j++) {
+console.log('i = ${i}, j = ${j}');
+}
+}
+for (let i = 1; i <= 4; i++) {
+    let stars = '';
+    for (let j = 1; j <= i; j++) {
+        stars += '*';
+    }
+    console.log(stars);
+}
